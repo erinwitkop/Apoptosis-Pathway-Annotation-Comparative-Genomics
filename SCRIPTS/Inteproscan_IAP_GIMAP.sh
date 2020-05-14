26aa4e322bb51df43ec14ee3f2f3509f9a9ac6f6
Erin Roberts <erin_roberts@my.uri.edu>
2020-05-13 23:18:24 -0400

    added script to run Interproscan to look at domains

--- SCRIPTS/Inteproscan_IAP_GIMAP.sh
+++ SCRIPTS/Inteproscan_IAP_GIMAP.sh
@@ -17,10 +17,11 @@ module load InterProScan/5.33-72.0-foss-2018b
 
 # Run InterProScan
 interproscan.sh -i $H/BIR_hmmsearch_XP_seq.fa -d $H/ -f GFF3,TSV
+interproscan.sh -i $H/AIG1_hmmsearch_XP_seq.fa -d $H/ -f GFF3,TSV
 
 # -i is the input data
 # -b is the output file base
 # -d is the output directory, the output filenames are the same as the input filename
-# -f is formats 
+# -f is formats
 
 echo "DONE $(date)"
